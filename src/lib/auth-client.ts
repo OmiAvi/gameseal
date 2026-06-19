689e96821b4f06cc1c32bffdac6f0c89a0fb6441
omeraviram <omeraviram@ufl.edu>
2026-06-19 10:45:14 -0400

    Fix Better Auth client base URL

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,6 +2,5 @@ import { createAuthClient } from 'better-auth/react'
 import { usernameClient } from 'better-auth/client/plugins'
 
 export const authClient = createAuthClient({
-  baseURL: '/api/auth',
   plugins: [usernameClient()],
 })
